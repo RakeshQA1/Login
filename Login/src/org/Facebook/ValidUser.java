83283c16901d7e8668b8aee02a5d1f4e7d85756b
Rakesh <Rakesh@DESKTOP-8RQ4G9A.mshome.net>
2024-07-29 16:55:43 +0100

    Second Commit

---
+++ Login/src/org/Facebook/ValidUser.java
@@ -0,0 +1,16 @@
+package org.Facebook;
+
+public class ValidUser {
+	
+	private void summa() {
+		System.out.println("Good Boy");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		ValidUser v = new ValidUser();
+		
+	}
+
+}
